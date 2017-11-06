ccd915b6998a84c088a372273bfc421c97dc5870
Yu Li <raise.sail@gmail.com>
2017-11-06 21:52:57 +0800

    [PATCH 12/18] [lkl] add cmpxchge implementation to prepare for SMP support
    This patch adds x86_64 platform support, these implementation
    are copied from arch/x86 directory for a while, may be a better
    solution for us later ?
    
    Signed-off-by: Yu Li <raise.sail@gmail.com>

--- arch/lkl/include/asm/cmpxchg.h
+++ arch/lkl/include/asm/cmpxchg.h
@@ -1,52 +1,10 @@
-/*
- * Generic UP xchg and cmpxchg using interrupt disablement.  Does not
- * support SMP.
- */
+#ifndef ASM_LKL_CMPXCHG_H
+#define ASM_LKL_CMPXCHG_H
 
-#ifndef __ASM_LKL_CMPXCHG_H
-#define __ASM_LKL_CMPXCHG_H
-
-#include <linux/types.h>
-#include <linux/irqflags.h>
-
-#ifndef xchg
-
-/*
- * This function doesn't exist, so you'll get a linker error if
- * something tries to do an invalidly-sized xchg().
- */
-extern void __xchg_called_with_bad_pointer(void);
-
-static inline
-unsigned long __xchg(unsigned long x, volatile void *ptr, int size)
-{
-	return x;	//FIXME
-}
-
-#define xchg(ptr, x) ({							\
-	((__typeof__(*(ptr)))						\
-		__xchg((unsigned long)(x), (ptr), sizeof(*(ptr))));	\
-})
-
-#endif /* xchg */
-
-/*
- * Atomic compare and exchange.
- */
-#include <asm-generic/cmpxchg-local.h>
-
-#ifndef cmpxchg_local
-#define cmpxchg_local(ptr, o, n) ({					       \
-	((__typeof__(*(ptr)))__cmpxchg_local_generic((ptr), (unsigned long)(o),\
-			(unsigned long)(n), sizeof(*(ptr))));		       \
-})
+#ifdef CONFIG_LKL_X86_64
+#include <asm/x86/cmpxchg.h>
+#else
+#error "oops, cmpxchg impl is missed."
 #endif
 
-#ifndef cmpxchg64_local
-#define cmpxchg64_local(ptr, o, n) __cmpxchg64_local_generic((ptr), (o), (n))
-#endif
-
-#define cmpxchg(ptr, o, n)	cmpxchg_local((ptr), (o), (n))
-#define cmpxchg64(ptr, o, n)	cmpxchg64_local((ptr), (o), (n))
-
-#endif /* __ASM_LKL_CMPXCHG_H */
+#endif	/* ASM_LKL_CMPXCHG_H */
